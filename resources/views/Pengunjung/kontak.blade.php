8e34c4e27d6cf19083a660fd3e1708fd7460bcd0
GalihFitria <galihfitria11@gmail.com>
2025-09-25 15:25:29 +0700

    crud

--- resources/views/Pengunjung/kontak.blade.php
+++ resources/views/Pengunjung/kontak.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INNDESA - Kontak</title>
+    <title>INNDESA - Inovasi Nusantara Desa Integratif Pangan</title>
     <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
 
     <!-- Open Graph Meta Tags for Social Media -->
